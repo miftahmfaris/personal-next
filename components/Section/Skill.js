1d6b610f97ada2a263e55ccc9a4c952a2e1c886e
Miftah Faris <miftah.faris07@yahoo.co.id>
2020-08-01 10:57:32 +0700

    add skill

--- components/Section/Skill.js
+++ components/Section/Skill.js
@@ -1,8 +1,51 @@
+import { Container, Row, Col } from "reactstrap";
+import styles from "../../styles/Skill.module.css";
+
 const Skill = () => {
     return (
-        <div>
-            <h1>Skillset</h1>
-        </div>
+        <Container fluid={true}>
+            <Row>
+                <Col xs="12" md="6" lg="6" className={styles.displayCenterLeft}>
+                    <h1>Skillset</h1>
+                </Col>
+                <Col
+                    xs="12"
+                    md="6"
+                    lg="6"
+                    className={`${styles.displayCenterRight} ${styles.skill}`}
+                >
+                    <ul style={{ listStyle: "square" }}>
+                        <li>
+                            <h5>HTML5</h5>
+                        </li>
+                        <li>
+                            <h5>CSS3</h5>
+                        </li>
+                        <li>
+                            <h5>Javascript</h5>
+                        </li>
+                        <li>
+                            <h5>React</h5>
+                        </li>
+                        <li>
+                            <h5>React-Redux</h5>
+                        </li>
+                        <li>
+                            <h5>NextJS</h5>
+                        </li>
+                        <li>
+                            <h5>NodeJS</h5>
+                        </li>
+                        <li>
+                            <h5>MongoDB-Mongoose</h5>
+                        </li>
+                        <li>
+                            <h5>mySQL-Sequelize</h5>
+                        </li>
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
