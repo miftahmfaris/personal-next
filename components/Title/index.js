814b814d25a36adfa27694d803853036eafc05c1
Miftah Faris <miftah.faris07@yahoo.co.id>
2020-08-02 05:59:09 +0700

    add pwa

--- components/Title/index.js
+++ components/Title/index.js
@@ -3,8 +3,32 @@ import Head from "next/head";
 const HeadComponent = ({ title }) => {
     return (
         <Head>
+            <meta charset="utf-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta
+                name="viewport"
+                content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+            />
+            <meta name="description" content="Description" />
+            <meta name="keywords" content="Keywords" />
+
             <title>{title}</title>
             <link rel="icon" href="/favicon.ico" />
+            <link rel="manifest" href="/manifest.json" />
+            <link
+                href="/favicon-16x16.png"
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+            />
+            <link
+                href="/favicon-32x32.png"
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+            />
+            <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+            <meta name="theme-color" content="#317EFB" />
         </Head>
     );
 };
